45064dda3d9dad414949f33c606326874916b4f8
Felice Forby <feliceforby@gmail.com>
2018-10-27 05:30:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,13 @@ class Post < ActiveRecord::Base
     where(author: author_id)
   end
 
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+  end
+
   private
 
   def is_title_case
